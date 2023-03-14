ebca5729982ea4316aa0626a327c99de90dc6e7a
imadnanhassan~ <abulhassan.dev@gmail.com>
2023-03-14 20:13:16 +0600

    array & object destructuring

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+// array
+const x = [1, 2, 3, 4, 5];
+const [y, z] = x;
+console.log(y); // 1
+console.log(z); // 2
+
+// object
+
+const obj = { a: 1, b: 2 };
+const { a, b } = obj;
+// is equivalent to:
+// const a = obj.a;
+// const b = obj.b;
